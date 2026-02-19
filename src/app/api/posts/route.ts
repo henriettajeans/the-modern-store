0429dac6b977a39bef159303a260d111a439fc46
henrietta jeansson <henriettajeansson@gmail.com>
2026-02-19 12:00:15 +0100

    add GET and POST to api route posts/route

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,50 @@
+import { IPost } from "@/models/IPost";
+import { getPosts, writePosts } from "@/services/postService";
+import { NextRequest, NextResponse } from "next/server";
+import slugify from "slugify";
+
+export async function GET() {
+    try {
+        const posts = getPosts();
+        return NextResponse.json(posts);
+    } catch (error) {
+        console.error("Error att ladda inlägg -", error);
+        return NextResponse.json(
+            { message: "Failed att ladda posterna" },
+            { status: 500 }
+        );
+    }
+}
+
+export async function POST(request: NextRequest) {
+    try {
+        const body = await request.json();
+        const { title, content } = body;
+
+        if (!title || !content) {
+            return NextResponse.json(
+                { message: "Add title and content, required!" },
+                { status: 400 }
+            );
+        }
+
+        const newPost: IPost = {
+            id: uuidv4(),
+            title, content, likes: 0, slug: slugify(title, { lower: true }),
+        };
+        const posts = getPosts();
+        posts.push(newPost);
+        writePosts(posts);
+        return NextResponse.json(newPost, { status: 201 });
+    } catch (error) {
+        console.error("Error att skapa post -", error);
+        return NextResponse.json(
+            { message: "Failed att skapa post" },
+            { status: 500 }
+        );
+    }
+}
+
+function uuidv4(): string {
+    throw new Error("Function not implemented.");
+}
--- src/services/postService.ts
+++ src/services/postService.ts
@@ -1,7 +1,18 @@
+import { IPost } from "@/models/IPost";
 import fs from "fs";
- import path from "path";
- import slugify from "slugify";
+import path from "path";
+import slugify from "slugify";
 
- const filePath = path.join(process.cwd(), "src", "data", "posts.json");
+const filePath = path.join(process.cwd(), "src", "data", "posts.json");
 
- 
+export const getPosts = (): IPost[] => {
+    const data = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+    return data.map((post: IPost) => ({
+        ...post,
+        slug: post.slug || slugify(post.title, { lower: true }),
+    }));
+};
+
+export const writePosts = (posts: IPost[]): void => {
+    fs.writeFileSync(filePath, JSON.stringify(posts, null, 2));
+};
